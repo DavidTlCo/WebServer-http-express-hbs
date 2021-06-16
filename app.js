e88b1ec203d23ca8fc1300713e4820bb2d63b8d7
David_TC <davidcoraza0316@gmail.com>
2021-06-16 08:51:28 -0500

    Webserver implements handlebars to host a website

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
-const port = 8080;
+require('dotenv').config();
+const port = process.env.PORT;
+
+// Handlebars
+// Cargando componentes posibles a reutilizar
+hbs.registerPartials( __dirname+'/views/partials')
+app.set('view engine', 'hbs');
 
 // Servir contenido estático usando middlewares
 app.use( express.static('public') );
 
-app.get( '/', (req, res) => {
-    res.sendFile(__dirname+'/public/index.html');
+
+// Servir el contenido del home page y demás endpoints cargando los archivos correspondientes con hbs
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'David Tlahupa Coraza',
+        titulo: 'Curso de Node'
+    });
+});
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre: 'David Tlahupa Coraza',
+        titulo: 'Curso de Node'
+    });
 });
 
-app.get( '/generic', (req, res) => {
-    res.sendFile(__dirname+'/public/generic.html');
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre: 'David Tlahupa Coraza',
+        titulo: 'Curso de Node'
+    });
 });
 
-app.get( '/elements', (req, res) => {
-    res.sendFile(__dirname+'/public/elements.html');
+app.get('*', (req, res) => {
+    res.render('error', {
+        nombre: 'David Tlahupa Coraza',
+        titulo: 'Curso de Node'
+    });
 });
 
-app.get( '*', (req, res) => {
-    res.sendFile(__dirname+'/public/404.html');
-});
 
 app.listen( port, () => {
     console.log( `Server listening at port ${port}` );
